0833c2e56c859349cd957c7bec9f213f73659ee4
jochen84 <simman@rocketmail.com>
2021-03-29 12:19:44 +0200

    enter custom file/filepath

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,16 +4,17 @@ import org.example.model.Tag;
 
 import java.io.*;
 import java.util.List;
+import java.util.Scanner;
 
 public class App {
 
     public static void main( String[] args ) {
 
-        //Scanner sc = new Scanner(System.in);
-        //System.out.println("Enter file you want to convert:");
-        //String filePath = sc.nextLine();
-        //File inputFile = new File(filePath);
-        File inputFile = new File("input/konvertera1.txt");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter file you want to convert: (Just press enter to use konvertera2.txt in input folder");
+        String filePath = sc.nextLine();
+        filePath = filePath.isEmpty() ? "input/konvertera2.txt" : filePath;
+        File inputFile = new File(filePath);
 
         CreateNewXmlFileName convertFileName = new CreateNewXmlFileName();
         String xmlFileName = convertFileName.getNewFileName(inputFile.getName());
